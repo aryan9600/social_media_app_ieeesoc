d2df3bd97131644a382fd023473bfef435a08f05
aryan9600 <sanskar.jaiswal2018@vitstudent.ac.in>
2019-06-05 13:41:42 +0530

    made some changes to the login page

--- lib/login.dart
+++ lib/login.dart
@@ -1,17 +1,3 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -37,8 +23,8 @@ class _LoginPageState extends State<LoginPage> {
                 SizedBox(height: 120.0),
                 TextField(
                   decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.blueAccent[100],
+                    //filled: true,
+                    //fillColor: Colors.blueAccent[100],
                     labelText: "Username"
                   ),
                   controller: _usernameController,
@@ -46,7 +32,7 @@ class _LoginPageState extends State<LoginPage> {
                 SizedBox(height: 50.0),
                 TextField(
                   decoration: InputDecoration(
-                    filled: true,
+                    //filled: true,
                     labelText: "Password"
                   ),
                   controller: _passwordController,
@@ -71,7 +57,16 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 Container(
                   padding: const EdgeInsets.fromLTRB(0.0, 6.0, 8.0, 0.0),
-                  child: Text('Forgot Password', style: TextStyle(fontSize: 14.0), textAlign: TextAlign.right,)
+                  child: Text('Forgot Password?', style: TextStyle(fontSize: 14.0), textAlign: TextAlign.right,)
+                ),
+                SizedBox(height: 100.0),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 8.0),
+                  child: Text('Dont have an account?', style: TextStyle(fontSize: 14.0), textAlign: TextAlign.center),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 8.0),
+                  child: Text('Sign up', style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold, decoration: TextDecoration.underline), textAlign: TextAlign.center),
                 )
           ],
         ),
